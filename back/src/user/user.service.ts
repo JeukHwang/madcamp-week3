3fdb7651eefcebafa36ebd5ca752018b16c5c1f8
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-14 07:06:54 +0900

    fix: add utility functions and .env.template

--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateDto } from './dto/create.dto';
 
 export type UserProfile = Pick<User, 'id' | 'name' | 'photo'>;
-const toUserProfile = (user: User): UserProfile => ({
+export const toUserProfile = (user: User): UserProfile => ({
   id: user.id,
   name: user.name,
   photo: user.photo,
@@ -13,38 +14,68 @@ const toUserProfile = (user: User): UserProfile => ({
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
-  async create(userInfo: CreateDto): Promise<UserProfile> {
+  async create(userInfo: CreateDto): Promise<User> {
     try {
-      const user = await this.prismaService.user.create({
+      const user: User = await this.prismaService.user.create({
         data: { ...userInfo },
       });
-      return toUserProfile(user);
+      return user;
     } catch (e) {
       throw e;
     }
   }
 
-  async findById(id: string): Promise<UserProfile | null> {
-    const user = await this.prismaService.user.findUnique({
+  async findById(id: string): Promise<User | null> {
+    const user: User | null = await this.prismaService.user.findUnique({
       where: { id },
     });
-
-    return user ? toUserProfile(user) : null;
+    return user;
   }
 
-  async findByGoogleId(googleId: string): Promise<UserProfile | null> {
-    const user = await this.prismaService.user.findUnique({
+  async findByGoogleId(googleId: string): Promise<User | null> {
+    const user: User | null = await this.prismaService.user.findUnique({
       where: { googleId },
     });
-
-    return user ? toUserProfile(user) : null;
+    return user;
   }
 
   async getLeaderboard(): Promise<UserProfile[]> {
-    const users = await this.prismaService.user.findMany({
+    const users: User[] | null = await this.prismaService.user.findMany({
       orderBy: [{ score: 'desc' }, { createdAt: 'asc' }],
       take: 5,
     });
     return users.map(toUserProfile);
+  }
+
+  async getUserIfRefreshTokenMatches(
+    id: string,
+    refreshToken: string,
+  ): Promise<User | null> {
+    const user: User | null = await this.findById(id);
+    if (user && user.refreshToken) {
+      const isRefreshTokenMatching = await bcrypt.compare(
+        refreshToken,
+        user.refreshToken,
+      );
+      if (isRefreshTokenMatching) {
+        return user;
+      }
+    }
+    return null;
+  }
+
+  async setRefreshToken(id: string, refreshToken: string): Promise<void> {
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+    await this.prismaService.user.update({
+      where: { id },
+      data: { refreshToken: hashedRefreshToken },
+    });
+  }
+
+  async removeRefreshToken(id: string): Promise<void> {
+    await this.prismaService.user.update({
+      where: { id },
+      data: { refreshToken: null },
+    });
   }
 }
