8317abbb3692e4fae2857a40a34253d9963f8cca
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-14 15:03:58 +0900

    fix: enable cors provided from nest

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,12 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import * as cors from 'cors';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.use(cors({}));
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
   await app.listen(port);
