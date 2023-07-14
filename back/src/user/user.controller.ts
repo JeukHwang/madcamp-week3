5cd5e40dbbed62767f4bc0b02d3b67cb23b6b920
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-14 12:23:42 +0900

    feat: add apis for finding user and leaderboard

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { CurrentUser } from './decorator/current.decorator';
+import { UserProfile, UserService, toUserProfile } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('current')
+  async getCurrentUser(@CurrentUser() user: User): Promise<UserProfile> {
+    return toUserProfile(user);
+  }
+
+  @Get('id/:id')
+  async getOneUser(@Param('id') id: string): Promise<UserProfile | null> {
+    const user = await this.userService.findById(id);
+    return user ? toUserProfile(user) : null;
+  }
+
+  @Get('leaderboard')
+  async getLeaderboard(): Promise<UserProfile[]> {
+    return await this.userService.getLeaderboard();
+  }
+}
