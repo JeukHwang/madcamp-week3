3a501c31a98d863703a808ae91a635073a6781e7
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-14 12:55:52 +0900

    feat: add cors setting for frontend

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(
+    cors({
+      origin: 'http://localhost:3001', // frontend
+      credentials: true,
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
   await app.listen(port);
