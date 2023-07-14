e0a8ad090c2d3da45d0fe469cdbec369405d566e
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-14 15:18:07 +0900

    fix: add options to cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
   await app.listen(port);
