f84243803693d7162ac7757a3afe6d39731c38b2
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-07-14 15:29:19 +0900

    edit

--- front/mad/src/pages/home/home.tsx
+++ front/mad/src/pages/home/home.tsx
@@ -9,16 +9,18 @@ import axios from "axios";
 const HomePage = () => {
     const movePage = useNavigate();
     const handleButtonClick = () => {
-        // API 호출 로직을 작성합니다.
-        // 예를 들어, axios를 사용하여 API를 호출하는 코드를 작성할 수 있습니다.
-        axios.get('/api/google-login')
-          .then(response => {
-            // API 호출에 성공한 경우 실행될 로직을 작성합니다.
-          })
-          .catch(error => {
-            // API 호출에 실패한 경우 실행될 로직을 작성합니다.
-          });
-      };
+      axios.get('https://madcamp-week3-production.up.railway.app/auth', { withCredentials: true })
+        .then(response => {
+          // API 호출에 성공한 경우 실행될 로직을 작성합니다.
+          console.log(response);
+          throw new Error('Network response was not ok.');
+        })
+        .catch(error => {
+          // API 호출에 실패한 경우 실행될 로직을 작성합니다.
+          console.error('API 호출에 실패했습니다.', error);
+        });
+    };
+    
       
     function main(){
         movePage('/main');
@@ -28,7 +30,7 @@ const HomePage = () => {
       <Area>
         <Background style={{ padding: "50px" }}>
           <Text size="5.0rem" color="white" font={Font.Bold} style={{padding:"50px"}}>
-            개발이 즐거워~
+            개발자 게임
           </Text>
           <Button onClick={main} variant={ButtonVariant.contained}>
             <Text size="3.0rem" font={Font.Medium}>나도!!</Text>
--- front/mad/src/pages/main/main.tsx
+++ front/mad/src/pages/main/main.tsx
@@ -3,15 +3,45 @@ import Text from "../../atoms/containers/text/text";
 import Font from "../../styles/font";
 import colorSet from "../../styles/colorSet";
 import Background from "../../atoms/containers/background/background";
-
+import React, { useState } from "react";
+import { useEffect } from "react";
+const width=7;
+const iconcolors = ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#4B0082", "#9400D3"]
+const icons = ["🍎", "🍊", "🍌", "🍉", "🍇", "🍓", "🍒"]
 const Main = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState<string[]>([]);
+
+
+  const createBoard = () => {
+  const randomcolorarrangement = []
+  for (let i = 0; i < width*width; i++) {
+    const randomColor = iconcolors[Math.floor(Math.random()*iconcolors.length)]
+    randomcolorarrangement.push(randomColor)
+  }
+  setCurrentColorArrangement(randomcolorarrangement)
+}
+
+useEffect(() => {
+createBoard()
+}, [width])
+
+
+console.log(currentColorArrangement)
   return (
             
       <Area>
-      <Background color={colorSet.red}>
-        <Text color="white" size="10rem" font={Font.Bold}>
-          진짜로 즐거워?
-        </Text>
+      <Background color={colorSet.white}>
+        <div className="app">
+          <div className="game">
+            {currentColorArrangement.map((iconcolors, index)=>(
+              <img
+                key = {index}
+                style = {{backgroundColor: iconcolors}}
+              />
+            ))}
+
+          </div>
+        </div>
         </Background>
       </Area>
   );
