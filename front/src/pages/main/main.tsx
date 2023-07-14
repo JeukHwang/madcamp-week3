31d65634dfab04c6ba82e57cc59da218d3a5a2fa
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-07-14 20:49:37 +0900

    toast popup

--- front/src/pages/main/main.tsx
+++ front/src/pages/main/main.tsx
@@ -14,6 +14,10 @@ import TypeScript from "../../assets/icon/typescript.svg";
 import Dart from "../../assets/icon/dart.svg";
 import Check from '../../assets/icon/programming.png';
 import "../../assets/effect/bouncingAnimation.css";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+
 const width=7;
 const iconimage = [
   Java
@@ -25,6 +29,26 @@ const iconimage = [
 const Main = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState<string[]>([]);
   const [selectedCells, setSelectedCells] = useState<[number, number][]>([]);
+
+  const sendSelectedCells = async () => {
+    if (selectedCells.length === 0) {
+      toast.warning("아무 것도 선택되지 않았습니다.", { autoClose: 1000 });
+      return;
+    }
+  
+    try {
+      // Make a POST request to the backend API with the selected cells data
+      await axios.post("/api/selected-cells", selectedCells);
+      
+      // Handle the success response or perform any necessary actions
+      console.log("Selected cells sent successfully");
+    } catch (error) {
+      // Handle any errors that occurred during the API request
+      console.error("Error sending selected cells:", error);
+    }
+  };
+  
+
   const createBoard = () => {
   const randomcolorarrangement = []
   for (let i = 0; i < width*width; i++) {
@@ -37,7 +61,7 @@ const Main = () => {
 const handleCellClick = (row: number, col: number) => {
   if (selectedCells.length === 3 && !isCellSelected(row, col)) {
     // Show warning popup when maximum selection limit is reached
-    alert("You can only select up to 3 cells.");
+    toast.warning("최대 3개까지 선택할 수 있습니다.", { autoClose: 1000 });
     return;
   }
 
@@ -49,7 +73,7 @@ const handleCellClick = (row: number, col: number) => {
       (Math.abs(prevRow - row) === 1 && Math.abs(prevCol - col) === 1)
     ) {
       // Show alert when the selected cell is not adjacent to the previous cell
-      alert("⚠ You can only select adjacent cells.");
+      toast.warning("연속된 셀만 선택할 수 있습니다.", { autoClose: 1000 });
       return;
     }
   }
@@ -64,6 +88,8 @@ const handleCellClick = (row: number, col: number) => {
     // Clicked on a new cell, add it to selected cells
     const updatedSelectedCells: [number, number][] = [...selectedCells, [row, col]];
     setSelectedCells(updatedSelectedCells);
+    console.log(row,col);
+    console.log(selectedCells);
 
     // Add the "bounce" class to the clicked cell
     const cell = document.getElementById(`cell-${row}-${col}`);
@@ -111,11 +137,16 @@ createBoard()
 
           </div>
         </div>
-        <Button>
+        <Button onClick={sendSelectedCells}>
           <Text font={Font.Bold} size={"5.0rem"} color={colorSet.gray}>
             check
           </Text>
         </Button>
+        <ToastContainer position="top-center" 
+        style={{ width: "400px" }}
+        className="custom-toast-container"
+        />
+
         </Background>
       </Area>
   );
