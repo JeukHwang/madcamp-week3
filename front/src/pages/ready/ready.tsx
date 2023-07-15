68445f118eb57639f92949b37455f9645575fd05
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-07-15 16:16:27 +0900

    game create connect

--- front/src/pages/ready/ready.tsx
+++ front/src/pages/ready/ready.tsx
@@ -15,7 +15,7 @@ const Ready = () => {
         console.error("API 호출에 실패했습니다.", error.response);
         if (error.response && error.response.status === 403 && error.response.data.message.startsWith("Game exist")) {
           const userId = error.response.data.message.split(" ").pop(); // 에러 메시지에서 사용자 ID 추출
-          history(`/game/${userId}`); // 사용자 ID를 가진 게임 페이지로 이동
+          history(`/game/update`); // 사용자 ID를 가진 게임 페이지로 이동
         } else {
           // 다른 에러 메시지인 경우나 메시지가 없는 경우에 대한 처리
           // 일반적인 에러 메시지를 표시하거나 다른 작업을 수행합니다.
