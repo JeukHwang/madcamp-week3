42ea988a7fe4e951d3916a91312a89d5c7af0ee1
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-15 16:47:53 +0900

    feat: applying logger middleware

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,16 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { LoggerMiddleware } from 'logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { JwtAccessGuard } from './auth/guard/jwt-access.guard';
+import { GameModule } from './game/game.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { GameModule } from './game/game.module';
 
 @Module({
   imports: [AuthModule, PrismaModule, UserModule, GameModule],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: JwtAccessGuard }],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
