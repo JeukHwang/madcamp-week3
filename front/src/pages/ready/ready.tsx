e331785a78ccffd0afd434b1443312431b45f2b1
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-07-15 16:09:36 +0900

    game create

--- front/src/pages/ready/ready.tsx
+++ front/src/pages/ready/ready.tsx
@@ -1,22 +1,33 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import { useNavigate } from "react-router-dom";
 
 const Ready = () => {
-    axios.get('https://madcamp-week3-production.up.railway.app/game/create', { withCredentials: true })
-    .then(response => {
-        // API 호출에 성공한 경우 실행될 로직을 작성합니다.
-        
+  const history = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("https://madcamp-week3-production.up.railway.app/game/create", { withCredentials: true })
+      .then(response => {
         console.log(response);
-    })
-    .catch(error => {
-        // API 호출에 실패한 경우 실행될 로직을 작성합니다.
-        console.error('API 호출에 실패했습니다.', error);
-    }); 
-    
-    return(
-        <div>
-            <h1>깅깅깅깅</h1>
-        </div>
-    )
-}
+      })
+      .catch(error => {
+        console.error("API 호출에 실패했습니다.", error.response);
+        if (error.response && error.response.status === 403 && error.response.data.message.startsWith("Game exist")) {
+          const userId = error.response.data.message.split(" ").pop(); // 에러 메시지에서 사용자 ID 추출
+          history(`/game/${userId}`); // 사용자 ID를 가진 게임 페이지로 이동
+        } else {
+          // 다른 에러 메시지인 경우나 메시지가 없는 경우에 대한 처리
+          // 일반적인 에러 메시지를 표시하거나 다른 작업을 수행합니다.
+        }
+      });
+  }, [history]);
+
+  return (
+    <div>
+      <h1>깅깅깅깅</h1>
+    </div>
+  );
+};
+
 export default Ready;
